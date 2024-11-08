9b962f422fa40aa68741bc51bb968708610ef7c2
lucasepe <luca.sepe@gmail.com>
2024-11-08 06:49:00 +0100

    fix: home dir windows

--- env/homedir_windows.go
+++ env/homedir_windows.go
@@ -4,12 +4,13 @@ package env
 
 import (
 	"errors"
+	"os"
 )
 
 // userHomeDir is the same as os.UserHomeDir, except that "getenv" is called instead of "os.Getenv"
 // (for caching). Also, the two switch statements are combined into just one.
 func userHomeDir() (string, error) {
-	value := getenv("USERPROFILE")
+	value := os.Getenv("USERPROFILE")
 	if value == "" {
 		return "", errors.New("%userprofile% is not defined")
 	}
