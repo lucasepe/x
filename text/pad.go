7c162e0d22dd38ee60168c54a5e476b00daace23
Luca Sepe <luca.sepe@gmail.com>
2025-09-23 07:42:33 +0200

    feat: consider utf8 strings in pad funcs

--- text/pad.go
+++ text/pad.go
@@ -2,30 +2,35 @@ package text
 
 import (
 	"bytes"
+
+	"github.com/mattn/go-runewidth"
 )
 
-// PadRight returns a new string of a specified length in which
+// PadRight returns a new string of a specified visual width in which
 // the end of the current string is padded with spaces or with a specified Unicode character.
-func PadRight(str string, length int, pad byte) string {
-	if len(str) >= length {
+func PadRight(str string, length int, pad rune) string {
+	w := runewidth.StringWidth(str)
+	if w >= length {
 		return str
 	}
-	buf := bytes.NewBufferString(str)
-	for i := 0; i < length-len(str); i++ {
-		buf.WriteByte(pad)
+	var buf bytes.Buffer
+	buf.WriteString(str)
+	for i := 0; i < length-w; i++ {
+		buf.WriteRune(pad)
 	}
 	return buf.String()
 }
 
-// PadLeft returns a new string of a specified length in which the
+// PadLeft returns a new string of a specified visual width in which the
 // beginning of the current string is padded with spaces or with a specified Unicode character.
-func PadLeft(str string, length int, pad byte) string {
-	if len(str) >= length {
+func PadLeft(str string, length int, pad rune) string {
+	w := runewidth.StringWidth(str)
+	if w >= length {
 		return str
 	}
 	var buf bytes.Buffer
-	for i := 0; i < length-len(str); i++ {
-		buf.WriteByte(pad)
+	for i := 0; i < length-w; i++ {
+		buf.WriteRune(pad)
 	}
 	buf.WriteString(str)
 	return buf.String()
